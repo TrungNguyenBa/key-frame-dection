4178564f9ab20f6082cea380a250d7ea32b8a71c
trung <trung@eduroamguest-2-180.sys.cs.local>
2020-02-13 14:26:12 -0500

    adding extraction based on timeframe

--- key-frame.py
+++ key-frame.py
@@ -3,9 +3,8 @@ import cv2
 import numpy as np
 
 video_path = sys.argv[1]
-method = sys.argv[2]
-threshold = float(sys.argv[3])
-timeframe = int(sys.argv[4])
+threshold = float(sys.argv[2])
+timeframe = int(sys.argv[3])
 
 # def absolute_diff(f1,f2):
 # 	diff = cv2.absdiff(f1,f2)
@@ -23,7 +22,9 @@ def histogram_diff(f1,f2):
 	ranges = h_ranges + s_ranges
 	channels = [0, 1]
 	f1 = cv2.calcHist([f1], channels, None, histSize, ranges, accumulate=False)
+	cv2.normalize(f1, f1, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX)
 	f2 = cv2.calcHist([f2], channels, None, histSize, ranges, accumulate=False)
+	cv2.normalize(f2, f2, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX)
 	diff = cv2.compareHist(f1, f2, 0)
 	if diff < threshold:
 		return True
@@ -34,21 +35,33 @@ compare = histogram_diff
 
 cap = cv2.VideoCapture(video_path)
 
-suc, prev_frame = cap.read()
+suc, frame = cap.read()
 ret = set()
 images = []
+m = {}
 while suc:
 	suc, curr_frame = cap.read()
-	
 	if suc:
-		diff = compare(curr_frame, prev_frame)
-		if diff :
-			s = int(cap.get(cv2.CAP_PROP_POS_MSEC)/1000)
-			if s not in ret:
-				images.append(curr_frame)
-				ret.add(s)
-	prev_frame = curr_frame
-
-for i,v in enumerate(images):
-	cv2.imwrite('images'+str(i)+'.png',v)
+		s = int(cap.get(cv2.CAP_PROP_POS_MSEC)/1000)
+		k = list(m.keys())	
+		for i in k:
+			if i < s - timeframe:
+				m.pop(i)
+		for i in m.get(s - timeframe,[]):
+			diff = compare(curr_frame,i)
+			if diff:
+				NotInclude = True
+				for i in range(0,timeframe):
+					if s - i in ret:
+						NotInclude = False
+				if NotInclude:
+					images.append(curr_frame)
+					ret.add(s)
+				break
+		m[s] = m.get(s,[])
+		m[s].append(curr_frame)
+print(len(images))
+print(sorted(ret))
+# for i,v in enumerate(images):
+# 	cv2.imwrite('images'+str(i)+'.png',v)
 
